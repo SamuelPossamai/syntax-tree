f60b2490a48ad9f9c61e71d215c2dae4b8c5a9a8
Samuel Possamai <possamai.sam@gmail.com>
2019-11-08 22:22:34 -0300

    node rules were separated from the syntax tree

--- syntax_tree.py
+++ syntax_tree.py
@@ -1,6 +1,55 @@
 
 import re
 
+class NodeRule:
+
+    def __init__(self, types, re_match, re_filter=None):
+
+        self.__types = set(types)
+        self.__re_match = re_match
+
+        if re_filter is None:
+            self.__re_filter = re_match
+        else:
+            self.__re_filter = re_filter
+
+    def applyRule(self, node):
+
+        if node.type_ not in self.__types:
+            return False
+
+        val = node.value
+
+        match = re.search(self.__re_match, val)
+
+        if match is None:
+            return False, ()
+
+        span = match.span()
+
+        match_string = val[span[0]:span[1]]
+
+        match_filtered = re.search(self.__re_filter, match_string)
+
+        if match_filtered is None:
+            return False, ()
+
+        filtered_span = match_filtered.span()
+
+        before = val[:span[0]+filtered_span[0]]
+        after = val[span[1]-(len(match_string) - filtered_span[1]):]
+        match_string = match_string[filtered_span[0]:filtered_span[1]]
+
+        node.value = match_string
+        child_before = SyntaxTreeElement(before)
+        child_after = SyntaxTreeElement(after)
+
+        node.addChild(child_before)
+        node.addChild(child_after)
+
+        return True, (child_before, child_after)
+
+
 class SyntaxTreeElement:
 
     def __init__(self, value, type_='Expression'):
@@ -71,15 +120,13 @@ class SyntaxTree:
         self.__expr = expr
         self.__root = SyntaxTreeElement(expr)
 
-        leafs = [self.__root]
+        leaves = [self.__root]
 
-        while(self.__split_single(leafs, r'\b\s*[+-]\s*\b', '[+-]')):
-            pass
+        for rule in self.__node_rules:
+            while(self.__apply_rule(leaves, rule)):
+                pass
 
-        while(self.__split_single(leafs, r'\b\s*[*/%]\s*\b', '[*/%]')):
-            pass
-
-        for leaf in leafs:
+        for leaf in leaves:
             for leaf_type, leaf_type_rule, leaf_type_filter in \
                 self.__leaf_rules:
 
@@ -99,43 +146,16 @@ class SyntaxTree:
                     f'Couldn\'t find a meaning for \'{leaf.value.strip()}\'')
 
     @staticmethod
-    def __split_single(leafs, symbol_match, symbol_filter):
+    def __apply_rule(leaves, rule):
 
         modified = False
 
-        for i, leaf in enumerate(leafs):
-            val = leaf.value
+        for i, leaf in enumerate(leaves):
 
-            match = re.search(symbol_match, val)
-
-            if match is None:
-                continue
-
-            span = match.span()
-
-            match_string = val[span[0]:span[1]]
-
-            match_filtered = re.search(symbol_filter, match_string)
-
-            if match_filtered is None:
-                continue
-
-            filtered_span = match_filtered.span()
-
-            before = val[:span[0]+filtered_span[0]]
-            after = val[span[1]-(len(match_string) - filtered_span[1]):]
-            match_string = match_string[filtered_span[0]:filtered_span[1]]
-
-            leaf.value = match_string
-            child_before = SyntaxTreeElement(before)
-            child_after = SyntaxTreeElement(after)
-
-            leaf.addChild(child_before)
-            leaf.addChild(child_after)
-
-            leafs[i:i + 1] = child_before, child_after
-
-            modified = True
+            applied, new_leaves = rule.applyRule(leaf)
+            if applied is True:
+                modified = True
+                leaves[i:i + 1] = new_leaves
 
         return modified
 
@@ -143,53 +163,20 @@ class SyntaxTree:
     def __repr__(self):
         return repr(self.__root)
 
-def split_single(leafs, symbol_match, symbol_filter):
-
-    modified = False
-
-    for i, leaf in enumerate(leafs):
-        val = leaf.value
-
-        match = re.search(symbol_match, val)
-
-        if match is None:
-            continue
-
-        span = match.span()
-
-        match_string = val[span[0]:span[1]]
-
-        match_filtered = re.search(symbol_filter, match_string)
-
-        if match_filtered is None:
-            continue
-
-        filtered_span = match_filtered.span()
-
-        before = val[:span[0]+filtered_span[0]]
-        after = val[span[1]-(len(match_string) - filtered_span[1]):]
-        match_string = match_string[filtered_span[0]:filtered_span[1]]
-
-        leaf.value = match_string
-        child_before = SyntaxTreeElement(before)
-        child_after = SyntaxTreeElement(after)
-
-        leaf.addChild(child_before)
-        leaf.addChild(child_after)
-
-        leafs[i:i + 1] = child_before, child_after
-
-        modified = True
-
-    return modified
-
 leaf_types = [
 
     ("Integer", "^\s*[0-9]+\s*$", "[0-9]+"),
     ("Word", "^\s*[A-Za-z_][A-Za-z_0-9]*\s*$", "[A-Za-z_][A-Za-z_0-9]*")
 ]
 
+node_rules = [
+
+    NodeRule(('Expression',), r'\b\s*[+-]\s*\b', re_filter='[+-]'),
+    NodeRule(('Expression',), r'\b\s*[*/%]\s*\b', re_filter='[*/%]')
+]
+
 syntax_tree = SyntaxTree('2 + 4 + 2*7 + 12 - 5',
-                         leaf_rules=leaf_types)
+                         leaf_rules=leaf_types,
+                         node_rules=node_rules)
 
 print(syntax_tree)
